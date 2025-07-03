c08420b60d38bcaaa4489a870989ccda075fa74a
Tomasz Styczeń <97025562+Tomek889@users.noreply.github.com>
2025-07-03 13:07:13 +0200

    Create routes to clubs, players and coaches pages

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -5,4 +5,16 @@ indexRouter.get("/", (req, res) => {
   res.send("inventory app");
 });
 
+indexRouter.get("/clubs", (req, res) => {
+  res.send("clubs");
+});
+
+indexRouter.get("/players", (req, res) => {
+  res.send("players");
+});
+
+indexRouter.get("/coaches", (req, res) => {
+  res.send("coaches");
+});
+
 module.exports = indexRouter;
