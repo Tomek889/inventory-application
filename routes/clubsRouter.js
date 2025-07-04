f61dcbe10c805de03e2ed4f2661e96606128f939
Tomasz Styczeń <97025562+Tomek889@users.noreply.github.com>
2025-07-04 13:35:49 +0200

    Create forms to edit and add new clubs

--- routes/clubsRouter.js
+++ routes/clubsRouter.js
@@ -15,4 +15,57 @@ clubsRouter.get("/", async (req, res) => {
   }
 });
 
+clubsRouter.get("/new", (req, res) => {
+  res.render("clubs/create", { title: "Add New Club" });
+});
+
+clubsRouter.post("/", async (req, res) => {
+  try {
+    const { name, founded_year, stadium_name, country } = req.body;
+    await clubsDB.createClub({ name, founded_year, stadium_name, country });
+    res.redirect("clubs");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error creating club");
+  }
+});
+
+clubsRouter.get("/:id/edit", async (req, res) => {
+  try {
+    const club = await clubsDB.getClubById(req.params.id);
+    if (!club) {
+      return res.status(404).send("Club not found");
+    }
+    res.render("clubs/edit", { title: "Edit Club", club: club });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error fetching club");
+  }
+});
+
+clubsRouter.post("/:id", async (req, res) => {
+  try {
+    const { name, founded_year, stadium_name, country } = req.body;
+    await clubsDB.editClub({
+      id: req.params.id,
+      name: name,
+      founded_year: founded_year,
+      stadium_name: stadium_name,
+      country: country,
+    });
+    res.redirect("/clubs");
+  } catch (err) {}
+  res.redirect("/");
+});
+
+clubsRouter.post("/:id/delete", async (req, res) => {
+  try {
+    await clubsDB.deleteClub(req.params.id);
+    res.redirect("/clubs");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error deleting club");
+  }
+});
+
 module.exports = clubsRouter;
--- routes/coachesRouter.js
+++ routes/coachesRouter.js
@@ -15,4 +15,57 @@ coachesRouter.get("/", async (req, res) => {
   }
 });
 
+coachesRouter.get("/new", (req, res) => {
+  res.render("coaches/create", { title: "Add New Coach" });
+});
+
+coachesRouter.post("/", async (req, res) => {
+  try {
+    const { name, role, age, club_id } = req.body;
+    await clubsDB.createClub({ name, founded_year, stadium_name, country });
+    res.redirect("clubs");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error creating club");
+  }
+});
+
+coachesRouter.get("/:id/edit", async (req, res) => {
+  try {
+    const club = await clubsDB.getClubById(req.params.id);
+    if (!club) {
+      return res.status(404).send("Club not found");
+    }
+    res.render("clubs/edit", { title: "Edit club", club: club });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error fetching club");
+  }
+});
+
+coachesRouter.post("/:id", async (req, res) => {
+  try {
+    const { name, founded_year, stadium_name, country } = req.body;
+    await clubsDB.editClub({
+      id: req.params.id,
+      name: name,
+      founded_year: founded_year,
+      stadium_name: stadium_name,
+      country: country,
+    });
+    res.redirect("/clubs");
+  } catch (err) {}
+  res.redirect("/");
+});
+
+coachesRouter.post("/:id/delete", async (req, res) => {
+  try {
+    await clubsDB.deleteClub(req.params.id);
+    res.redirect("/clubs");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error deleting club");
+  }
+});
+
 module.exports = coachesRouter;
