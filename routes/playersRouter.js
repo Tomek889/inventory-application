664abb480a9dfbff8d03e706933665c47d042c7a
Tomasz Styczeń <97025562+Tomek889@users.noreply.github.com>
2025-07-04 14:14:28 +0200

    Create forms to edit and add new players

--- routes/playersRouter.js
+++ routes/playersRouter.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
 const playersRouter = Router();
 const playersDB = require("../models/players");
+const clubsDB = require("../models/clubs");
 
 playersRouter.get("/", async (req, res) => {
   try {
@@ -12,6 +13,67 @@ playersRouter.get("/", async (req, res) => {
   } catch (err) {
     console.error(err);
     res.status(500).send("Error fetching players");
+  }
+});
+
+playersRouter.get("/new", async (req, res) => {
+  const clubs = await clubsDB.getAllClubs();
+  res.render("players/create", { title: "Add New Player", clubs: clubs });
+});
+
+playersRouter.post("/", async (req, res) => {
+  try {
+    const { name, position, age, club_id } = req.body;
+    await playersDB.createPlayer({ name, position, age, club_id });
+    res.redirect("/players");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error creating player");
+  }
+});
+
+playersRouter.get("/:id/edit", async (req, res) => {
+  try {
+    const player = await playersDB.getPlayerById(req.params.id);
+    const clubs = await clubsDB.getAllClubs();
+    if (!player) {
+      return res.status(404).send("Player not found");
+    }
+    res.render("players/edit", {
+      title: "Edit Player",
+      player: player,
+      clubs: clubs,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error fetching player");
+  }
+});
+
+playersRouter.post("/:id", async (req, res) => {
+  try {
+    const { name, position, age, club_id } = req.body;
+    await playersDB.editPlayer({
+      id: req.params.id,
+      name: name,
+      position: position,
+      age: age,
+      club_id: club_id,
+    });
+    res.redirect("/players");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error editing player");
+  }
+});
+
+playersRouter.post("/:id/delete", async (req, res) => {
+  try {
+    await playersDB.deletePlayer(req.params.id);
+    res.redirect("/players");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error deleting player");
   }
 });
 
